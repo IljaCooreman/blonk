a49a1ab68f79e5f0abaaf1cfe3f141c709a9aa4c
IljaCooreman <IljaCooreman@users.noreply.github.com>
2019-01-22 15:29:46 +0100

    Refactor folder structure (#3)
    * put auth-frontend in new folder
    
    * add demo route to frontend

--- mothership/auth-frontend/src/AuthContainer/index.js
+++ mothership/auth-frontend/src/AuthContainer/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import CircleButton from './CircleButton';
-import { postData } from '../lib/post';
+import { postData } from '../lib/requests';
+
 
 export default class AuthContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { token: this.createEmptyToken(), delay: 800, authenticated: false };
+    this.state = { token: this.createEmptyToken(), delay: 800, authenticated: false, serverError: '' };
     this.handleButtonClick = this.handleButtonClick.bind(this);
     this.handleSendClick = this.handleSendClick.bind(this);
-    this.validateAuthData = this.validateAuthData.bind(this);
+    this.validateAuthResponse = this.validateAuthResponse.bind(this);
+  }
+
+  componentDidMount() {
+    this.sendRequest(this.createEmptyToken())
+
   }
 
   createEmptyToken() {
@@ -24,11 +30,20 @@ export default class AuthContainer extends Component {
 
   sendRequest(token) {
     postData('http://localhost:9000/auth', { entryCode: token })
-      .then(this.validateAuthData);
+      .then(this.validateAuthResponse);
   };
 
-  async validateAuthData(response) {
-    if (!response.ok) return;
+  async validateAuthResponse(response) {
+    if (!response.ok) {
+      if (response.status >= 500 || response.status === 404) {
+        console.log(response)
+        response.json().then(data => {
+          this.setState({ serverError: data.error });
+        })
+      }
+      return;
+    };
+
     const data = await response.json();
     console.log(data)
     if (!data || !data.satelliteId || !data.token) return;
@@ -48,7 +63,11 @@ export default class AuthContainer extends Component {
   }
 
   render() {
-    const { token, authenticated } = this.state;
+    const { token, authenticated, serverError } = this.state;
+    if (serverError) return (
+      <div className={`circle-button-container`}>{serverError}</div>
+    );
+
     return (
       <div className={`circle-button-container`}>
         {
